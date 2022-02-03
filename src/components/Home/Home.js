ac24eb1eeb7b200dcd05ae38eeae44931c7d7579
lilith1995 <lilith.mnatsakanian@gmail.com>
2022-02-03 16:57:59 +0400

    Fixed bugs

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,32 +3,7 @@ import CircularIndeterminate from "../Loader/Loader";
 
 const Home = ({ onQuizStart }) => {
 
-  const [user, setUser] = useState({
-    id: "",
-    name: "",
-    email: ""
-  });
-
   const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const getCurrentUser = async (e) => {
-      var headers = {}
-      try {
-        const res = await fetch("/api/users/current", {
-          method: "GET",
-          mode: "cors",
-          headers: headers
-        })
-        console.info("RESPONSE", await res.clone().json());
-        const data = await res.json();
-        setUser([...data]);
-      } catch (e) {
-        console.error("failed to fetch", e);
-      }
-    };
-    getCurrentUser();
-  }, []);
 
   if (loading) {
     return (
@@ -38,8 +13,9 @@ const Home = ({ onQuizStart }) => {
   else {
     return (
       <div className="homepanel">
+
         <div className="homepanel_content">
-          <h1>Can you get the perfect score?{user.name}</h1>
+          <h1>Can you get the perfect score?</h1>
           <h2>Start and try!</h2>
           <button className="buttonstart" onClick={onQuizStart}>Check it out</button>
         </div>
